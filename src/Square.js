06b60bc22cdb16efcae56f980cc0de2973c42726
Andrea Castillo <andreacastillo@me.com>
2020-10-17 02:30:42 -0500

    KnownGood with function Square()

--- src/Square.js
+++ src/Square.js
@@ -1,31 +1,18 @@
 import React from 'react';
 
-// function Square(props) {
-//     return (
-//         <button
-//             className="square"
-//             onClick={props.onClick}
-//             style={{color: props.value === 'X' ? 'red' : 'blue'}}
-//         >
-//             {props.value}
-//         </button>
-//     );
-// }
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-                style={{
-                    color: this.props.value === 'X' ? 'red' : 'blue',
-                    backgroundColor: (this.props.winner && this.props.winner.winnerCombo.includes(this.props.index)) ? 'yellow' : 'white'
-                }}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={() => props.onClick()}
+            style={{
+                color: props.value === 'X' ? 'red' : 'blue',
+                backgroundColor: (props.winner && props.winner.winnerCombo.includes(props.index)) ? 'yellow' : 'white'
+            }}
+        >
+            {props.value}
+        </button>
+    );
 }
 
 export default Square;
